ae99909d459f8a76f65906c1f310c4799472ea80
vinithdevilvinith <vinithkumar31102004@gmail.com>
2025-12-23 09:41:29 +0530

    adding full statements

--- conditional.js
+++ conditional.js
@@ -215,38 +215,132 @@ for(initialize variable; condition;statement){
 }
 */
 
-for(i=1;i<=10;i++)
+// for(i=1;i<=10;i++)
+// {
+//     console.log(i);   
+// }
+
+// let arr=[];
+// for(i=0;i<100;i+=3)
+// {
+//     arr.push(i);
+// }
+// console.log(arr);
+// console.log(arr[33]);
+
+// // //Nested For Loop:
+// let nums=[];
+// for(let i=0;i<3;i++) //i=0 0<3 1<3
+// {
+//     nums.push([]); //nums[0] nums[1]
+//     for(let j=0;j<3;j++)
+//     {
+//         nums[i].push(j)// num[1]={0,1,2}
+//     }
+// }
+// console.log(nums);
+// console.table(nums);
+
+
+// let a = [];
+// a.push([]);     // row
+// a[0].push(10);  // column
+// console.log(a);
+
+// //For of Loop:
+// let names = ['Tiya','vani','vinith','vinitha','jothika'];
+
+// for(let i=0;i<names.length;i++)
+// {
+//     console.log(names[i]);
+    
+// }
+
+// console.log('For of Loop :');
+
+// for(let v of names){
+//     console.log(v);
+// }
+
+// // For in Loop:
+// let user = {
+//     name: "Vinith",
+//     age: 22,
+//     city: 'Salem',
+//     contact:'9876543210'
+// };
+
+// for(let prop in user)
+// {
+//     console.log(prop+" : "+user[prop]);
+    
+// }
+
+//Looping over objects by converting to an array
+
+let user = {
+    name: "Vinith",
+    age: 22,
+    city: 'Salem',
+    contact:'9876543210'
+};
+
+let arr_keys=Object.keys(user);
+console.table(arr_keys);
+let arr_values=Object.values(user);
+console.table(arr_values);
+
+for(let i=0;i<arr_keys.length;i++)
 {
-    console.log(i);   
+    console.log(arr_keys[i]+' : '+arr_values[i]);
+    //console.log(user[arr_keys[i]]);   
 }
 
-let arr=[];
-for(i=0;i<100;i+=3)
-{
-    arr.push(i);
-}
-console.log(arr);
-console.log(arr[33]);
+// Break Statement:
 
-// //Nested For Loop:
-let nums=[];
-for(let i=0;i<3;i++) //i=0 0<3 1<3
+for(let i=0;i<=10;i++)
 {
-    nums.push([]); //nums[0] nums[1]
-    for(let j=0;j<3;j++)
-    {
-        nums[i].push(j)// num[1]={0,1,2}
+    console.log(i);
+    if(i==4){
+        break;
+    }   
+}
+
+// Continue Statement:
+for(let i=0;i<=10;i++)
+{
+  
+    if(i==5){
+        continue;
     }
+      console.log(i)
 }
-console.log(nums);
-console.table(nums);
+
+for(let i=0;i<=10;i++)
+{
+  
+    if(i%2==0){
+        continue;
+    }
+      console.log(i)
+}
 
 
-let a = [];
-a.push([]);     // row
-a[0].push(10);  // column
-console.log(a);
+//Label Block in JavaScript:
+let groups = [
+    ['Ram','Sam','Ravi'],
+    ['Kumar','Tiya','Sundar'],
+    ['Rajesh','Sara','Rahul'],
+];
 
-
-
-
+for(let group of groups)
+{
+    inner:
+    for(let member of group)
+        {
+            if(member.startsWith('R')){
+                console.log('Found one Starting with R:',member);
+                break inner;
+            }    
+        }
+}
